f80676f61e83630e80993bc688837b185992d320
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2023-02-27 15:20:53 +0100

    pair?

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -2,11 +2,7 @@
 
 (defn suit [card]
   "Exercise 1: Write the function (suit card) which takes a singe card and
-   returns the suit of the card as a one character string.
-       (suit \"2H\") => \"H\"
-       (suit \"2D\") => \"D\"
-       (suit \"2C\") => \"C\"
-       (suit \"3S\") => \"S\""
+   returns the suit of the card as a one character string."
   (let [[_ s] card]
     (str s)))
 
@@ -17,14 +13,7 @@
 
 (defn rank [card]
   "Exercise 2: Write the function (rank card) which takes a single card and
-   returns the rank as a number between 2 and 14.
-       (rank \"2H\") => 2
-       (rank \"4S\") => 4
-       (rank \"TS\") => 10
-       (rank \"JS\") => 11
-       (rank \"QS\") => 12
-       (rank \"KS\") => 13
-       (rank \"AS\") => 14"
+   returns the rank as a number between 2 and 14."
   (let [[r _] card
         ranks [\2 \3 \4 \5 \6 \7 \8 \9 \T \J \Q \K \A]]
     (+ 2 (.indexOf ranks r))))
@@ -37,8 +26,29 @@
 (assert (= 13 (rank "KS")))
 (assert (= 14 (rank "AS")))
 
+(def high-seven                   ["2H" "3S" "4C" "5C" "7D"])
+(def pair-hand                    ["2H" "2S" "4C" "5C" "7D"])
+(def two-pairs-hand               ["2H" "2S" "4C" "4D" "7D"])
+(def three-of-a-kind-hand         ["2H" "2S" "2C" "4D" "7D"])
+(def four-of-a-kind-hand          ["2H" "2S" "2C" "2D" "7D"])
+(def straight-hand                ["2H" "3S" "6C" "5D" "4D"])
+(def low-ace-straight-hand        ["2H" "3S" "4C" "5D" "AD"])
+(def high-ace-straight-hand       ["TH" "AS" "QC" "KD" "JD"])
+(def flush-hand                   ["2H" "4H" "5H" "9H" "7H"])
+(def full-house-hand              ["2H" "5D" "2D" "2C" "5S"])
+(def straight-flush-hand          ["2H" "3H" "6H" "5H" "4H"])
+(def low-ace-straight-flush-hand  ["2D" "3D" "4D" "5D" "AD"])
+(def high-ace-straight-flush-hand ["TS" "AS" "QS" "KS" "JS"])
+
 (defn pair? [hand]
-  nil)
+  "Exercise 3: Write the function (pair? hand) that returns true if there is a
+   pair in hand and false if there is no pair in hand."
+  (let [ranks (map rank hand)
+        freqs (into #{} (vals (frequencies ranks)))]
+    (contains? freqs 2)))
+
+(assert (pair? pair-hand))
+(assert (not (pair? high-seven)))
 
 (defn three-of-a-kind? [hand]
   nil)
