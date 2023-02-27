72ee037bae0b5dd9f55a6250b5131dcd3d488433
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2023-02-27 14:52:57 +0100

    rank

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,7 +1,5 @@
 (ns p-p-p-pokerface)
 
-(defn assert [test] (if (false? test) (throw (AssertionError. "test failed"))))
-
 (defn suit [card]
   "Exercise 1: Write the function (suit card) which takes a singe card and
    returns the suit of the card as a one character string.
@@ -18,7 +16,26 @@
 (assert (= "S" (suit "3S")))
 
 (defn rank [card]
-  nil)
+  "Exercise 2: Write the function (rank card) which takes a single card and
+   returns the rank as a number between 2 and 14.
+       (rank \"2H\") => 2
+       (rank \"4S\") => 4
+       (rank \"TS\") => 10
+       (rank \"JS\") => 11
+       (rank \"QS\") => 12
+       (rank \"KS\") => 13
+       (rank \"AS\") => 14"
+  (let [[r _] card
+        ranks [\2 \3 \4 \5 \6 \7 \8 \9 \T \J \Q \K \A]]
+    (+ 2 (.indexOf ranks r))))
+
+(assert (= 2 (rank "2H")))
+(assert (= 4 (rank "4S")))
+(assert (= 10 (rank "TS")))
+(assert (= 11 (rank "JS")))
+(assert (= 12 (rank "QS")))
+(assert (= 13 (rank "KS")))
+(assert (= 14 (rank "AS")))
 
 (defn pair? [hand]
   nil)
