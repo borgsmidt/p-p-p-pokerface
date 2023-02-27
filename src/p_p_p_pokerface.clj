0ba91c160f057a5590263969bbeb7d050e13a425
Rasmus Borgsmidt <rasmus@borgsmidt.dk>
2023-02-27 14:39:29 +0100

    suit

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,9 +1,23 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn assert [test] (if (false? test) (throw (AssertionError. "test failed"))))
 
 (defn suit [card]
+  "Exercise 1: Write the function (suit card) which takes a singe card and
+   returns the suit of the card as a one character string.
+       (suit \"2H\") => \"H\"
+       (suit \"2D\") => \"D\"
+       (suit \"2C\") => \"C\"
+       (suit \"3S\") => \"S\""
+  (let [[_ s] card]
+    (str s)))
+
+(assert (= "H" (suit "2H")))
+(assert (= "D" (suit "2D")))
+(assert (= "C" (suit "2C")))
+(assert (= "S" (suit "3S")))
+
+(defn rank [card]
   nil)
 
 (defn pair? [hand]
